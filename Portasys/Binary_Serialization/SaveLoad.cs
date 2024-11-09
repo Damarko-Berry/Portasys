9f88d780f905b4f26041ab4de686407dd3d37526
Damarko Berry <markopolo269s@gmail.com>
2024-11-09 10:12:51 -0500

    Serialization added

---
+++ Portasys/Binary_Serialization/SaveLoad.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+
+namespace Portasys.Binary_Serialization
+{
+    public static class SaveLoad
+    {
+        public static void Save<T>(T obj, string ProjectName, string Filename)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            using (FileStream stream = new FileStream(Path.Combine(PortableEnviroment.GetFolder(SystemFolders.ProgramData), ProjectName, Filename), FileMode.Create, FileAccess.Write))
+            {
+                formatter.Serialize(stream, obj);
+            }
+            Console.WriteLine("Object serialized successfully.");
+        }
+
+        public static T Load <T>(string ProjectName, string Filename)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            using (FileStream stream = new FileStream(Path.Combine(PortableEnviroment.GetFolder(SystemFolders.ProgramData), ProjectName, Filename), FileMode.Open, FileAccess.Read))
+            {
+                var person = (T)formatter.Deserialize(stream);
+                Console.WriteLine("Object deserialized successfully.");
+                return person;
+            }
+        }
+
+
+    }
+}
---
+++ Portasys/Xml_Serialization/SaveLoad.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Portasys.Xml_Serialization
+{
+    public static class SaveLoad
+    {
+        public static void Save<T>(T obj, string ProjectName, string Filename)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+            var stream = new StreamWriter(Path.Combine(PortableEnviroment.GetFolder(SystemFolders.ProgramData), ProjectName, Filename));
+            serializer.Serialize(stream, obj);
+            stream.Close();
+        }
+
+        public static T Load <T>(string ProjectName, string Filename)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+            StreamReader sr = new StreamReader(Path.Combine(PortableEnviroment.GetFolder(SystemFolders.ProgramData), ProjectName, Filename));
+            var obj = (T)xmlSerializer.Deserialize(sr);
+            sr.Close();
+            return obj;
+        }
+    }
+}
